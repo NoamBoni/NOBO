f80697c919f9c7dad8982e7c716bd19e17320ccf
Noam Boni <noamboni55@gmail.com>
2021-10-07 09:17:55 +0300

    test

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { IconContext } from 'react-icons';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import './style/index.scss';
 import App from './App.jsx';
 import Theme from './style/Theme';
 
+const queryClient = new QueryClient();
+
 const Main = () => {
     return (
-        <IconContext.Provider value={{}}>
-            <BrowserRouter>
-                <Theme>
-                    <App />
-                </Theme>
-            </BrowserRouter>
-        </IconContext.Provider>
+        <QueryClientProvider client={queryClient}>
+            <IconContext.Provider value={{}}>
+                <BrowserRouter>
+                    <Theme>
+                        <App />
+                    </Theme>
+                </BrowserRouter>
+            </IconContext.Provider>
+        </QueryClientProvider>
     );
 };
 
